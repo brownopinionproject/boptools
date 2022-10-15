61f6302472cb8f99233f7fb84789ad34bd761ccf
arjun-shanmugam <65803362+arjun-shanmugam@users.noreply.github.com>
2022-10-15 16:57:53 -0400

    Created package structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='boptools',
+      version='0.1',
+      description='Brown Opinion Project utilities.',
+      url='https://github.com/brownopinionproject/boptools/tree/main',
+      author='Arjun Shanmugam',
+      author_email='arjun_shanmugam@brown.edu',
+      license='MIT',
+      pacckages=['boptools'],
+      zip_safe=False)
